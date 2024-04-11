c80a13b4b66cc51098572d7fa20c1c324de5cefe
blackninja707 <haruki.tan98@gmail.com>
2024-04-11 12:18:31 +0800

    Font Fix and Spacing Fix

--- src/components/dashboard/howItWorksFragment.tsx
+++ src/components/dashboard/howItWorksFragment.tsx
@@ -4,7 +4,7 @@ const HowItWorksFragment = () => {
     const { t } = useTranslation();
 
     return (
-        <div className="pt-16 px-16 md:px-24 lg:px-36">
+        <div className="pt-16 px-16 font-Barlow md:px-24 lg:px-36">
             <div className="justify-center relative items-center flex">
                 <div className="text-primaryText hidden md:flex md:flex-col max-w-xl">
                     <p className="text-white font-Barlow text-[16px]">{t("howItWorks.howItWorks")}</p>
@@ -19,9 +19,9 @@ const HowItWorksFragment = () => {
                     <p className="text-white font-Barlow text-[12px] -translate-y-4">{t("howItWorks.howItWorks").toUpperCase()}</p>
                     <div className="flex flex-col text-3xl">
                         <span className="text-gradient font-bold">{t("howItWorks.smartContracts")}</span>
-                        <span className="font-normal text-lg">&nbsp;{t("howItWorks.and")}</span>
+                        <span className="font-normal text-2xl">&nbsp;{t("howItWorks.and")}</span>
                         <span className="text-gradient font-bold">&nbsp;{t("howItWorks.SYXRewards")}</span>
-                        <span className="font-normal text-lg"> {t("howItWorks.fuelOurTech")}</span>
+                        <span className="font-normal text-2xl"> {t("howItWorks.fuelOurTech")}</span>
                     </div>
                 </div>
                 <div className="-z-10 absolute md:relative w-64 md:w-full inset-0 translate-x-32 m-auto opacity-60 md:max-w-lg">
